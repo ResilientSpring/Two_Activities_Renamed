32a6ac97e2e077e0681cd73c09c56637f238194c
ted10014 <ted10014@gmail.com>
2020-10-05 02:28:45 +0800

    Committed 2020/10/4 as a makeup schedule for 2020/10/3 (Saturday).

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -6,22 +6,101 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = findViewById(R.id.editText_main);
+
+
+        // Use findViewByID() to get references from the layout to the reply header and reply TextView elements.
+        // Assign those view instances to the private variables
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
 
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        String message = mMessageEditText.getText().toString();
+
+        /*
+        * getText():
+        * Get and return string from the textview, and to be stored as type of Charsequence
+        *
+        * toString():
+        * Convert Charsequence into String type
+        *
+        * References:
+        * 1. https://developer.android.com/reference/android/widget/TextView#getText()
+        * 2. https://developer.android.com/reference/java/lang/String#toString()
+        * 3. https://web.archive.org/web/20201004125944/https://stackoverflow.com/questions/47150909/what-does-gettext-tostring-return
+        * 4. https://web.archive.org/web/20201004125946/https://stackoverflow.com/questions/50539062/textview-gettext-tostring-vs-textview-tostring
+        * */
+
+        // Add that String message to the Intent as an extra
+        // with the constant "EXTRA_MESSAGE" as the key and the string "message" as the value
+        intent.putExtra(EXTRA_MESSAGE, message);
+        //  startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    // Add a public constant to define the key for a particular type of response you're interested in.
+    public static final int TEXT_REQUEST = 1;
+
+    // Add two private variables to hold the reply header and reply TextView elements, respectively.
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    /*
+    The three arguments to onActivityResult() contain all the information you need to handle the return data:
+    the requestCode you set when you launched the Activity with startActivityForResult(),
+    the resultCode set in the launched Activity (usually one of RESULT_OK or RESULT_CANCELED), and
+    the Intent data that contains the data returned from the launch Activity. */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Add code to test for TEXT_REQUEST to make sure you process the right Intent result,
+        // in case there are several.
+        if (requestCode == TEXT_REQUEST) {
+
+            // Also test for RESULT_OK, to make sure that the request was successful.
+            if (resultCode == RESULT_OK) {
+
+                // get the Intent extra from the response Intent (data).
+                // Here the key for the extra is the constant EXTRA_REPLY from SecondActivity.
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                // Set the visibility of the reply header to true.
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                // Set the reply TextView text to the reply, and set its visibility to true.
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+
+            }
+        /**
+         * The Activity class defines the result codes.
+         * The code can be RESULT_OK (the request was successful),
+         * RESULT_CANCELED (the user cancelled the operation),
+         * or RESULT_FIRST_USER (for defining your own result codes).
+         * */
+
+        }
+
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,13 +2,71 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+// The response data from the second Activity back to the main Activity is sent in an Intent extra.
+// You construct this return Intent and put the data into it in much the same way you do for the sending Intent.
 
 public class SecondActivity extends AppCompatActivity {
+
+    // add a public constant to define the key for the Intent extra
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"; // URL
+
+    // Add a private variable at the top of the class to hold the EditText.
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Get the Intent that activated this Activity.
+        Intent intent = getIntent();
+
+        // Get the string containing the message from the Intent extras
+        // using the MainActivity.EXTRA_MESSAGE static variable as the key
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Use findViewByID() to get a reference to the TextView for the message from the layout
+        TextView textView = findViewById(R.id.text_message);
+
+        // Set the text of the TextView to the string from the Intent extra
+        textView.setText(message);
+
+        // use findViewByID() to get a reference to the EditText and assign it to that private variable mReply
+        mReply = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+
+        // get the text of the EditText as a string
+        String reply = mReply.getText().toString();
+
+        // create a new intent for the response—don't reuse the Intent object that you received from the original request.
+        Intent replyIntent = new Intent();
+
+        /**
+         * When you use an explicit Intent to start another Activity,
+         * you may not expect to get any data back—you're just activating that Activity.
+         * In that case, you use startActivity() to start the new Activity.
+         *
+         * If you want to get data back from the activated Activity (SecondActivity), however,
+         * you need to start it (in MainActivity) with startActivityForResult().
+         * */
+
+        // Add the reply string from the EditText to the new intent as an Intent extra.
+        // Because extras are key/value pairs, here the key is EXTRA_REPLY, and the value is the reply
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+
+        // Set the result to RESULT_OK to indicate that the response was successful.
+        setResult(RESULT_OK, replyIntent);
+        // The Activity class defines the result codes, including RESULT_OK and RESULT_CANCELLED.
+
+        // Call finish() to close the Activity and return to MainActivity.
+        finish();
     }
 }
